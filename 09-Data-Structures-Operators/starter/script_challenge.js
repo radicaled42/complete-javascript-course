92bd2df5b9eb6b4d0a7a94f64fad0b65f054f0e7
radicaled <daniel.bianco@outlook.com>
2022-07-14 21:39:02 -0300

    completed section 9

--- 09-Data-Structures-Operators/starter/script_challenge.js
+++ 09-Data-Structures-Operators/starter/script_challenge.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 const game = {
   team1: 'Bayern Munich',
@@ -259,7 +259,7 @@ HINT 4: This challenge is difficult on purpose, so start watching the solution i
 Afterwards, test with your own test data!
 
 GOOD LUCK 😀
-*/
+
 
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
@@ -280,3 +280,63 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
   }
 });
+
+*/
+
+///////////////////////////////////////
+// String Methods Practice
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const cleanUpFlights = flights.toLowerCase().split('+');
+// console.log(cleanUpFlights);
+
+for (const flight of cleanUpFlights) {
+  const [status, origin, destination, time] = flight.split(';');
+  const [state, procedure] = status.replace('_', '').split('_');
+
+  const stateUpper = state.replace(state[0], state[0].toUpperCase());
+  let procedureUpper = '';
+  const originAirport = origin.slice(0, 3).toUpperCase();
+  const destinationAirport = destination.slice(0, 3).toUpperCase();
+
+  if (state === 'delayed') {
+    procedureUpper = procedure.replace(
+      procedure[0],
+      procedure[0].toUpperCase()
+    );
+    console.log(
+      `🔴 ${stateUpper} ${procedureUpper} from ${originAirport} to ${destinationAirport} (${time.replace(
+        ':',
+        'h'
+      )})`.padStart(36)
+    );
+  } else {
+    procedureUpper = state.replace(state[0], state[0].toUpperCase());
+    console.log(
+      `${procedureUpper} from ${originAirport} to ${destinationAirport} (${time.replace(
+        ':',
+        'h'
+      )})`.padStart(36)
+    );
+  }
+}
+
+// Official resolution
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
+  console.log(output);
+}
